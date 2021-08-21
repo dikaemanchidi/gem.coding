04cc23f7c6c7af4b1c53262a847f9c531805650c
dikaemanchidi <81798427+dikaemanchidi@users.noreply.github.com>
2021-08-22 02:37:00 +0900

    Update slack-notifier-notification.rb

--- slack-notifier-notification.rb
+++ slack-notifier-notification.rb
@@ -1,3 +1,3 @@
 require 'slack-notifier'
 notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
-notifier.ping "Hello World from #{dikae}"
+notifier.ping "Hello World from #{dikaemanchidi}"
